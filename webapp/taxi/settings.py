b95f3a350fbea6148faeacad0ad49fa9b627c934
robinsonweng <robinsonweng100@gmail.com>
2024-06-19 09:18:58 +0800

    Adding config for swagger ui

--- webapp/taxi/settings.py
+++ webapp/taxi/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 
     # installed libaries, tools
     'rest_framework',
+    'drf_spectacular',
 
     # django apps
     'core',
@@ -159,8 +160,18 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'core.User'
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
+
+AUTH_USER_MODEL = 'core.User'
 
 REDIS_URL = f"{environment_variable.redis.host}:{environment_variable.redis.port}"
 
