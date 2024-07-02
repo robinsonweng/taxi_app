103239631997c5c3577dbaa30251cdcd5072b856
robinsonweng <robinsonweng100@gmail.com>
2024-07-02 08:55:30 +0800

    Add setting to use namespace versioning DRF

--- webapp/taxi/settings.py
+++ webapp/taxi/settings.py
@@ -163,6 +163,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
 }
 
 SPECTACULAR_SETTINGS = {
