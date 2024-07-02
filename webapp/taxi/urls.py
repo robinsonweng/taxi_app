9759e72da9f63ee6f91be41d2ef613c73338ea02
robinsonweng <robinsonweng100@gmail.com>
2024-07-02 08:52:51 +0800

    add version & routing for swagger

--- webapp/taxi/urls.py
+++ webapp/taxi/urls.py
@@ -21,6 +21,6 @@ from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/schema/', SpectacularAPIView.as_view(api_version="v1"), name='schema-v1'),
+    path('api/v1/schema/swagger-ui', SpectacularSwaggerView.as_view(url_name='schema-v1'), name='swagger-ui-v1'),
 ]
